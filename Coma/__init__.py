e99264df72ec3e1d40b61082fc5e6a5864094799
Emile Clark-Boman <eclarkboman@gmail.com>
2024-05-25 03:45:27 +1000

    Coma instance will handle allowed providers

--- Coma/__init__.py
+++ Coma/__init__.py
@@ -2,25 +2,16 @@ from typing import List
 
 # import requests
 
-from Coma.Core.Component import Component
-
-from Coma.Core.Providers.GitHub import GitHub
-from Coma.Core.Providers.GitLab import GitLab
-from Coma.Core.Providers.BitBucket import BitBucket
+from Coma.Types import Component
+from Coma.Providers import Provider
 
 class Coma:
-    PROVIDERS = {
-        'GitHub': GitHub(),
-        'GitLab': GitLab(),
-        'BitBucket': BitBucket()
-    }
-
-    def __init__(self) -> None:
-        pass
+    def __init__(self, providers: List[Provider]) -> None:
+        self.providers = {provider.name: provider for provider in providers}
 
     def NewComponent(self, name: str, author: str) -> Component:
         component = Component(name, author)
-        component.providers = dict.fromkeys(self.PROVIDERS)
+        component.providers = dict.fromkeys(self.providers)
         return component
 
     '''
@@ -29,9 +20,15 @@ class Coma:
     '''
     def FillProviders(self, component: Component) -> List[str]:
         for provider_name in component.providers.keys():
-            provider = self.PROVIDERS[provider_name]
+            provider = self.providers[provider_name]
             component.providers[provider_name] = provider.Exists(component)
 
 
-    def GetReleases():
+    def GetReleases(self):
         pass
+
+
+    # Check to see if the data returned by all providers
+    # for the component is the same (matches)
+    def CheckProvidersMatch(self, component: Component) -> bool:
+        return None

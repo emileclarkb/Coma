e6828cf7522079ccfac279b7626dffa2fd8293a9
Emile Clark-Boman <eclarkboman@gmail.com>
2024-05-27 11:17:07 +1000

    using githubapi as a testing environment

--- Coma/Providers/GitHubApi.py
+++ Coma/Providers/GitHubApi.py
@@ -1,5 +1,5 @@
 import requests
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from Coma.Types import Author, Branch, Commit, Release, Component
 from Coma.Providers.ProviderApi import Provider, ProviderData
@@ -8,8 +8,8 @@ from Coma.Providers.ProviderApi import Provider, ProviderData
 class GitHubData(ProviderData):
     name = 'GitHub'
     def __init__(self, clone_url: str, website: str, owner: Author, 
-                       num_contributors: int,
-                       contributors: List[Author]) -> None:
+                       default_branch: str, contributors: List[Author],
+                       num_contributors: int) -> None:
         super().__init__()
         
         # TODO: provider = GITHUB() OBJECT REFERENCE HERE
@@ -20,6 +20,15 @@ class GitHubData(ProviderData):
         self.owner = owner
         self.contributors = contributors
         self.num_contributors = num_contributors
+        
+        '''
+        Variables beyond this section are not necessary for the app,
+        but they are necessary internally. So ProviderData objects
+        might need to have an alternative class that handles their
+        data being cropped and given to the app.
+        '''
+        
+        self.default_branch = default_branch
         
         # the maximum number of contributors we're willing to display
         # TODO: make this a config option OUTSIDE of Coma, 
@@ -42,7 +51,7 @@ class GitHub(Provider):
     @staticmethod
     def GetData(component: Component) -> Optional[GitHubData]:
         path = 'https://api.github.com/repos/{}/{}'
-        path = path.format(component.name, component.author)
+        path = path.format(component.author, component.name)
         result = requests.get(path)
         if not result.ok: return None
         content = result.json()        
@@ -50,21 +59,22 @@ class GitHub(Provider):
         clone_url = content['clone_url']
         website = content['html_url']
         owner = GitHub.ParseAuthor(content['owner'])
+        default_branch = content['default_branch']
         contrib_url = content['contributors_url']
         contributors, num_contributors = GitHub.GetContributors(contrib_url)
-        data = GitHubData(clone_url, website, owner, 
+        data = GitHubData(clone_url, website, owner, default_branch,
                           contributors, num_contributors)
         
     # The contributor list INCLUDES the owner
     @staticmethod
-    def GetContributors(url: str, max=None) -> tuple[List[Author], int]:
-        contrib_result = requests.get()
-        if not contrib_result.ok: return None
-        contrib_content = contrib_result.json()
+    def GetContributors(url: str, max=None) -> Optional[tuple[List[Author], int]]:
+        result = requests.get(url)
+        if not result.ok: return None
+        content = result.json()
         
-        num_contributors = len(contrib_content)
+        num_contributors = len(content)
         contributors = []
-        for i, contributor in enumerate(contrib_content):
+        for i, contributor in enumerate(content):
             # we've reached the maximum number of 
             # contributors we're allowed to parse
             if max is not None and i+1 >= max:
@@ -73,10 +83,11 @@ class GitHub(Provider):
         return contributors, num_contributors
                 
     @staticmethod
-    def GetReleases(component: Component) -> List[Release]:
+    def GetReleases(component: Component) -> Optional[List[Release]]:
         path = 'https://api.github.com/repos/{}/{}/tags'
-        path = path.format(component.name, component.author)
+        path = path.format(component.author, component.name)
         result = requests.get(path)
+        if not result.ok: return None
 
         releases = []
         for release in result.json():
@@ -90,9 +101,9 @@ class GitHub(Provider):
         return releases
     '''
     def GetReleases(self, component: Component) -> List[Release]:
-        name, author = component.Unpack()
-        url = 'https://api.github.com/repos/{}/{}/releases'
-        result = requests.get(url.format(name, author))
+        path = 'https://api.github.com/repos/{}/{}/releases'
+        path = path.format(component.author, component.name)
+        result = requests.get(path)
 
         releases = []
         for release in result.json():
@@ -105,10 +116,11 @@ class GitHub(Provider):
     '''
     
     @staticmethod
-    def GetBranches(component: Component) -> List[Branch]:
+    def GetBranches(component: Component) -> Optional[List[Branch]]:
         path = 'https://api.github.com/repos/{}/{}/branches'
-        path = path.format(component.name, component.author)
+        path = path.format(component.author, component.name)
         result = requests.get(path)
+        if not result.ok: return None
 
         branches = []
         for branch in result.json():
@@ -121,3 +133,29 @@ class GitHub(Provider):
             except KeyError as e:
                 print(e)
         return branches
+    
+    @staticmethod
+    def GetFileContent(component: Component, branch: str, 
+                       filename: str) -> Optional['json']:
+        path = 'https://raw.githubusercontent.com/{}/{}/{}/{}'
+        path = path.format(component.author, component.name, branch, filename)
+        result = requests.get(path)
+        return result.content if result.ok else None          filename: str) -> Optional['json']:
+        
+    
+    @staticmethod
+    def GetReadme(component: Component, branch: str) -> Optional[str]:
+        result = GitHub.GetFileContent(component, branch, 'README.md')
+        if result is None:
+            result = GitHub.GetFileContent(component, branch, 'README')
+        
+    '''
+    # TODO: reference ProviderData.default_branch instead of taking paramater
+    @staticmethod
+    def GetMetadata(component: Component, branch: str, 
+                    file_name: str) -> Optional[Union[tuple, list]]:
+        path = 'https://raw.githubusercontent.com/{}/{}/{}/{}'
+        path = path.format(component.author, component.name, branch, file_name)
+        result = requests.get(path)
+        return result.content if result.ok else None
+    '''

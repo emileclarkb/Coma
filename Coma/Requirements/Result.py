9ed37a94dd9d33d99a364e93b16a565714a14f37
Emile Clark-Boman <eclarkboman@gmail.com>
2024-05-27 19:00:10 +1000

    allow multiple Result reasons

--- Coma/Requirements/Result.py
+++ Coma/Requirements/Result.py
@@ -1,20 +1,25 @@
-from typing import Any
+from typing import Any, List
 
 
 class Result:
-    def __init__(self, success: bool, value: Any, reason=None) -> None:
+    def __init__(self, success: bool, value: Any, 
+                       reasons: str | List[str] = None) -> None:
         self.success = success
         self.value = value
-        self.reason = reason    # the reason we failed (optional)
+        # the reasons we failed (optional)
+        if reasons is not None and not isinstance(reasons, list):
+            self.reasons = [reasons]
+        else:
+            self.reasons = reasons
 
-    # ie. if result: blah blah blah :)
+    # ie. "if result: blah blah blah" :)
     def __bool__(self):
         return self.success
 
     @classmethod
-    def Succeed(cls, value: Any, reason='ok') -> 'Result':
-        return cls(True, value, reason=reason)
+    def Succeed(cls, value: Any, reasons: str | List[str] = 'ok') -> 'Result':
+        return cls(True, value, reasons=reasons)
     
     @classmethod
-    def Fail(cls, reason: str, value=None) -> 'Result':
-        return cls(False, value, reason=reason)
+    def Fail(cls, reasons: str | List[str], value=None) -> 'Result':
+        return cls(False, value, reasons=reasons)
